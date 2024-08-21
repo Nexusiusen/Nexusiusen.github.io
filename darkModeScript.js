3716fd16aeae43148d061e842af42ebec85df98c
Nexusiusen <Nexus.wikblad@gmail.com>
2024-08-21 08:40:43 +0200

    finalized changes

--- darkModeScript.js
+++ darkModeScript.js
@@ -1,5 +1,16 @@
 function myFunction() 
-        {
-            var element = document.body;
-            element.classList.toggle("dark-mode");  
-        }
+{
+    var element = document.body;
+    element.classList.toggle("dark-mode"); 
+
+   if (icon.src.includes('moonIcon.png')) 
+   {
+        icon.src = 'assets/sunIcon.png';
+    } 
+
+    else 
+    {
+        icon.src = 'assets/moonIcon.png';
+    }
+
+}
